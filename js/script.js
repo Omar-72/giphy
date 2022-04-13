a8e6aca9cf3fa61c8cc5b3152fe09336e38d1f97
Glitch (omar-giphy) <none>
2022-04-13 22:25:17 +0000

    ☝️🚎 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-let puppyRuns="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+let puppyRuns="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
 
 $(".search-button").click(function(){
   fetch(puppyRuns)
   
   .then(function(response){
     return response.json();
+      
   })
   
-  .then(function(data){
-    console.log(data.url);
-  })
+ .then(function(data){
+    console.log(data[0].images.original.mp4);
+    $(".main").append(`<img src="${data[0].images.original.mp4}">`);
+  });
 });
 
 
