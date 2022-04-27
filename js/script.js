c35095139df239687b37578fd8fb015a58c83e98
Glitch (omar-giphy) <none>
2022-04-27 22:23:33 +0000

    🏅🥟 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-let puppyRuns="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
 
 $(".search-button").click(function(){
+   
+  let search=$(".search-term").val();
+  let puppyRuns=`https://api.giphy.com/v1/gifs/search?q=${search}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`;
+
+  
   fetch(puppyRuns)
   
   .then(function(response){
@@ -9,10 +13,31 @@ $(".search-button").click(function(){
   })
   
  .then(function(data){
-    console.log(data[0].images.original.mp4);
-    $(".main").append(`<img src="${data[0].images.original.mp4}">`);
+    console.log(data.data[0].images.original.url);
+    
+    let gif=data.data[0].images.original.url
+  let show=(`<img src="${gif}"/>`);
+    
+    
+     $(".main").html(show);
+  
+    
   });
 });
 
+let ranPet=`https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`
 
+fetch(ranPet)
+  
+  .then(function(response){
+    return response.json();
+      
+  })
+  
+ .then(function(data){
+    console.log(data.data[0].images);
+  let allPet= data.data[0].images.original
+  
+$(".main").html(`<img src="${allPet}"/>`);
 
+});
